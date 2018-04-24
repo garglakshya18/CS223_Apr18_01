31fb29e18da347ebfa04fd0d8ba3196c01ff8d0c
Lakshya Garg <garg.2@iitj.ac.in>
2018-04-24 19:07:08 +0530

    Code refactored.

---
+++ code/cmake-build-debug/CMakeFiles/CMS.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lakshya/CLionProjects/CMS/code/src/Auth/User.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/Auth/User.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/Conference Details/Conference.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/Conference_Details/Conference.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/Database/File.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/Database/File.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/Init.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/Init.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/Registration/Payment.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/Registration/Payment.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/View/Menu.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/View/Menu.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/View/Views.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/View/Views.cpp.o"
+  "/home/lakshya/CLionProjects/CMS/code/src/main.cpp" "/home/lakshya/CLionProjects/CMS/code/cmake-build-debug/CMakeFiles/CMS.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "User"
+  "Conference"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/cmake-build-debug/CMakeFiles/CMS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMS.dir/src/main.cpp.o"
+  "CMakeFiles/CMS.dir/src/Init.cpp.o"
+  "CMakeFiles/CMS.dir/src/Auth/User.cpp.o"
+  "CMakeFiles/CMS.dir/src/Conference_Details/Conference.cpp.o"
+  "CMakeFiles/CMS.dir/src/Registration/Payment.cpp.o"
+  "CMakeFiles/CMS.dir/src/View/Views.cpp.o"
+  "CMakeFiles/CMS.dir/src/View/Menu.cpp.o"
+  "CMakeFiles/CMS.dir/src/Database/File.cpp.o"
+  "CMS.pdb"
+  "CMS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
